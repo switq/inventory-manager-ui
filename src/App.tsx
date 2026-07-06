fbb1540a30d7dfd00e27a160500805ca0e3e0b3d
Bolos-e-Doces <felipe2003g@gmail.com>
2026-07-05 22:17:45 -0300

    Simbora Iiiiiiha

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
+          SIMBORA IIIIIHA
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
